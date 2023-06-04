347ab8449b69369c59df7e74e79283bbd5e3ea89
Sagar <dev.sagartmg@gmail.com>
2023-06-04 10:30:39 +0545

    variables

---
+++ index.js
@@ -0,0 +1,125 @@
+console.log("Hello World");
+
+
+/* 
+    Comment 
+     ctrl + /      line comment
+     alt + shift + a   block comment
+*/
+
+/* 
+    printing in different programming languages
+        print("print in python")
+        System.out.println("hello java");
+*/
+
+/* 
+    Variables  / container / box
+        - word  which can store certain value
+
+
+    javascript syntax
+        name = "ram"
+
+    java
+        String name = "ram"
+    
+    php
+        $name = "ram"
+
+
+ */
+
+greetings = "Namaste"
+name = "Ram"
+age = 20
+
+console.log(greetings, name)
+console.log(name, age)
+
+brand = "apple"
+console.log(brand);
+
+fullname = "John mayer"
+console.log(fullname);
+
+
+/* 
+    RESERVED Keywords 
+        - cannot create variables via them
+*/
+
+course_name = "Mern"  // snake_case
+courseDuration = 3   // camelCase
+course_price = 15000
+coursePrice = 15000
+
+number1 = "One"
+console.log(number1);
+
+
+projector_color = "white"
+projectorColor = "white"
+
+productPrice = 100.50
+
+isPaid = true
+isMarried = false
+
+/*  
+        DATA-TYPES
+            1. String - text
+            2. Number 
+                - integer
+                - dobule / float / decimal 
+            3. Boolean 
+                - true
+                - false
+
+*/
+
+
+/*      
+    to declare/create  variable , use Keyword first 
+        - var ( donot use it.  )
+        - let ( most preferred )
+        - const
+*/
+
+
+var laptopBrand = 'acer'
+console.log("laptop brand is", laptopBrand);
+
+let laptopColor = "black"  // declartion and initilation 
+console.log("laptop color is", laptopColor);
+
+const PI = 3.14
+console.log("the value of pi is", PI);
+
+
+/*  DECLARATION vs INITIALZITION  */
+
+var mobileBrand // declartion
+var mobileBrand = "samsung"  // initialization
+mobileBrand = "apple"  // re-initialization
+
+console.log(mobileBrand);
+
+
+let watchBrand   // declartion
+watchBrand = "casio" // initialization
+watchBrand = "swiss"  // re-initialization
+console.log(watchBrand);
+
+const EARTH_ROTATION = "east-to-west";
+// EARTH_ROTATION = "west-to-east";
+console.log(EARTH_ROTATION);
+
+
+
+
+
+
+
+
+
