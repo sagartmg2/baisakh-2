7b882a10d4933be4a51b915d8884fb83adb06608
Sagar <dev.sagartmg@gmail.com>
2023-06-08 18:05:58 +0545

    function

---
+++ function.js
@@ -0,0 +1,74 @@
+// let students = [
+//    ,
+//     ,
+//     {
+//         index: 2,
+//         name: "ram",
+//         phone: 333,
+//         country: "nepal"
+//     }
+// ]
+
+
+let student1 = {
+    index: 0,
+    name: "ram",
+    phone: 1111,
+    country: "usa"
+}
+
+let student2 = {
+    index: 1,
+    name: "Hari",
+    phone: 222,
+    country: "uk"
+}
+
+let student_3 = {
+    index: 2,
+    name: "shyam",
+    phone: 333,
+    country: "nepal"
+}
+
+/* ram's phone is 111 */
+
+
+
+
+// console.log("Namaste", name);
+// console.log("Hello", name);
+// console.log("Nihao", name);
+// console.log("Konichiwa", name);
+// let name = "John"
+
+// function greetHim(greeting) { // let greeting = Namste
+//     console.log(greeting, name);
+// }
+
+// greetHim("Namste")
+// greetHim("Hello")
+// greetHim("Nihao")
+
+
+let person1 = "ram"
+let person2 = "Hari"
+let person3 = "shyam"
+
+
+// console.log("Namaste", person1, " ", "Namaste", person2, "", "Namaste", person3);
+// console.log("hello", person1, " ", "hello", person2, "", "hello", person3);
+// console.log("Nihao", person1, " ", "Nihao", person2, "", "Nihao", person3);
+
+
+function greetPeople(greeting) {  // greeting here is parameter
+
+    console.log(greeting, person1, " ", greeting, person2, "", greeting, person3);
+
+}
+
+greetPeople("Namaste"); // Namste here is arguement
+greetPeople("Hello"); // Hello here is arguement
+
+
+/*  */
---
+++ sum.js
@@ -0,0 +1,66 @@
+/* Wap to calculate sum of two  numbers
+
+    1, 2
+    3, 3
+    3, 10
+    10, 10
+    10, 100
+
+    the sum of 1 and 2 is 3 
+    .
+    .
+    .
+    the sum of 3 and 10 is 13
+
+
+
+    1+2 = 3
+    10+10  = 20
+*/
+
+// console.log("the sum of 1 and 2 is ", 1 + 2);
+// console.log("the sum of 3 and 3 is ", 3 + 3);
+// console.log(`the sum of 10 and 10 is ${10 + 10} `); // template litreal
+// console.log(`the sum of 110 and 10 is ${110 + 10} `);
+
+// console.log(" 1 + 2 = ", 1 + 2);
+// console.log(" 3 + 3 = ", 3 + 3);
+// console.log(` 10 + 10 = ${10 + 10} `); // template litreal
+// console.log(`110 + 10 = ${110 + 10} `);
+
+
+/* shift + alt + A */
+
+/*
+    - Donot repeat yourself 
+    - create once use again and agin.
+
+    FUNCTIONS 
+
+function  <functionName> ( ) { 
+        do some task...
+ }
+
+*/
+
+function sum(num1, num2) {  // num1 = 3; num2 = 4
+    console.log(`The sum of ${num1} and ${num2} is ${num1 + num2}`);
+}
+
+/* execute the function  or calling the function */
+sum(10, 4)
+sum(10, 10)
+sum(100, 10)
+
+
+// function doSomething () {
+//     console.log("do something")
+// } 
+
+// doSomething()
+// doSomething()
+// doSomething()
+// doSomething()
+
+
+
