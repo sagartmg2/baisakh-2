d07ef70ad466ee292364ee75762331fb17dda7e5
Sagar <dev.sagartmg@gmail.com>
2023-06-22 18:08:28 +0545

    todolist and localstorage

---
+++ falsy-values.js
@@ -0,0 +1,15 @@
+
+/* falsy values
+    false
+    null
+    undefined
+    0
+    NaN
+    "" // empty string
+*/
+
+if(""){
+    console.log("true condition");
+}else{
+    console.log("false condition");
+}
